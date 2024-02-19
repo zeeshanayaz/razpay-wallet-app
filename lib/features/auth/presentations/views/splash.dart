8997bb838c11098547638e9dd13e58ab1fb1de4d
zeeshanayaz <zeeshanayaz1@gmail.com>
2024-02-20 00:44:47 +0500

    Fade animation added on splash screen

--- lib/features/auth/presentations/views/splash.dart
+++ lib/features/auth/presentations/views/splash.dart
@@ -1,8 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:razpay/core/colors.dart';
 import 'package:razpay/core/constant.dart';
-import 'package:razpay/core/style.dart';
 import 'package:razpay/core/utils/shared_pref.dart';
 import 'package:razpay/router.dart';
 
@@ -14,8 +15,16 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  double _opacity = 0.0;
+
   @override
   void initState() {
+    Timer(const Duration(seconds: 1), () {
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
+
     Future.delayed(
       const Duration(seconds: 3),
       () {
@@ -33,11 +42,11 @@ class _SplashScreenState extends State<SplashScreen> {
     } else {
       String hasToken = await SharedPref.getString(kToken) ?? '';
       // bool hasLoggedUser = await SharedPref.getBool(kLoggedUser) ?? false;
-      if(hasToken.isNotEmpty) {
+      if (hasToken.isNotEmpty) {
         // if(hasLoggedUser) {
         //   Get.offAllNamed(AppRoutes.mainHome);
         // } else {
-          Get.offAndToNamed(AppRoutes.passcode);
+        Get.offAndToNamed(AppRoutes.passcode);
         // }
       } else {
         Get.offAndToNamed(AppRoutes.login);
@@ -53,13 +62,26 @@ class _SplashScreenState extends State<SplashScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Center(
+          /*Center(
               child: Text(
             'Razpay'.toUpperCase(),
             style: headerStyle.copyWith(
               color: white,
             ),
-          )),
+          )),*/
+
+          Center(
+            child: AnimatedOpacity(
+              opacity: _opacity,
+              duration: const Duration(seconds: 3),
+              // The green box must be a child of the AnimatedOpacity widget.
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+          )
         ],
       ),
     );
